401866f297eca6161280529432c96ba6967ecb94
Vanshaj Gupta <116676851+vanshajgupta37@users.noreply.github.com>
2023-10-02 02:15:36 +0530

    Add basicJavascript.js (#17)
    Adding the basic, commonly used code snippet of Javascript.
    
    Signed-off-by: guntarakesh <guntarakesh971@gmail.com>

---
+++ Web-Development/JAVASCRIPT/basicJavascript.js
@@ -0,0 +1,99 @@
+// This is a single-line comment. It's ignored by the JavaScript interpreter.
+
+/*
+  This is a multi-line comment.
+  It can span multiple lines and is also ignored by JavaScript.
+*/
+
+// Variables and Data Types
+
+// Declare a variable using 'var', 'let', or 'const'.
+let age = 30; // 'let' allows reassignment.
+const name = "First"; // 'const' creates a constant variable.
+
+// Data types: Number, String, Boolean, Array, Object, null, undefined
+const num = 42;
+const greeting = "Hello, World!";
+const isTrue = true;
+const fruits = ["apple", "banana", "cherry"];
+const person = {
+  firstName: "First",
+  lastName: "Second",
+};
+
+// Functions
+
+// Declare a function with 'function' keyword.
+function add(a, b) {
+  return a + b;
+}
+
+// Declare a function using a function expression.
+const subtract = function (a, b) {
+  return a - b;
+};
+
+// Arrow functions (ES6+)
+const multiply = (a, b) => a * b;
+
+// Conditional Statements
+
+if (age >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a minor.");
+}
+
+// Loops
+
+for (let i = 0; i < 5; i++) {
+  console.log(`Iteration ${i}`);
+}
+
+// Arrays
+
+fruits.push("orange"); // Add an element to the end of an array.
+fruits.pop(); // Remove the last element from an array.
+
+// Objects
+
+console.log(person.firstName); // Access object properties.
+
+// DOM Manipulation (browser-specific)
+
+// Select an HTML element by its ID.
+const element = document.getElementById("myElement");
+
+// Event handling (e.g., click event)
+element.addEventListener("click", function () {
+  console.log("Element clicked!");
+});
+
+// Promises (ES6+)
+
+const fetchData = () => {
+  return new Promise((resolve, reject) => {
+    // Simulate fetching data (e.g., from an API).
+    setTimeout(() => {
+      const data = { message: "Data received" };
+      resolve(data); // Resolve the promise with data.
+    }, 2000);
+  });
+};
+
+fetchData()
+  .then((data) => {
+    console.log(data.message);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+// Error Handling (try...catch)
+
+try {
+  // Code that may throw an error.
+  throw new Error("An error occurred.");
+} catch (error) {
+  console.error(error.message);
+}
