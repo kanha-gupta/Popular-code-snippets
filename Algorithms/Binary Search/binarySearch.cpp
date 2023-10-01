2ae00a199d090576b9558cc82c8ead46e56e347c
Mudit Jain <muditert34@gmail.com>
2023-10-01 21:27:18 +0530

    binary search

---
+++ Algorithms/Binary Search/binarySearch.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binary_search(vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2; //to prevent integer overflow
+
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int target = 5;
+    int result = binary_search(arr, target);
+
+    if (result != -1) {
+        cout << "Element " << target << " found at index " << result << endl;
+    } else {
+        cout << "Element " << target << " not found in the array" << endl;
+    }
+
+    return 0;
+}
