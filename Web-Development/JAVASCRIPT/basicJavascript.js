7698dd309e9fdf5760b73c6c2eb04014cd2f65f9
Mima Silva <mima@futurelab.co.nz>
2023-10-03 18:18:18 -0300

    Comments explaining wwhat the codes do.
    Signed-off-by: Mima Silva <mima@futurelab.co.nz>

--- Web-Development/JAVASCRIPT/basicJavascript.js
+++ Web-Development/JAVASCRIPT/basicJavascript.js
@@ -98,9 +98,12 @@ try {
   console.error(error.message);
 }
 
-//Copy to clipboard using class 'anchor'
+//Add smooth anchor scroll using class 'anchor'
 $(".anchor").on("click", function() {
-  var anchorTo = $(this).attr("href");
+  //Get the href property value
+  let anchorTo = $(this).attr("href");
+  //Calculation of the spacing necessary for the object with ID to appear correctly
   let offset = ($(window).width() < 960) ? 200 : 100;
+  //Makes the animation following the calculation for the object that contains the ID with the same href value
   $("html,body").animate({ scrollTop: $(anchorTo).offset().top - offset}, "slow");
 });
