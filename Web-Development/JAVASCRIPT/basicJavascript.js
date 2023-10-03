4f237315a35df44181389704cd32b5b2eebd2458
Mima Silva <mima@futurelab.co.nz>
2023-10-03 18:26:00 -0300

    feat: anchor smooth

--- Web-Development/JAVASCRIPT/basicJavascript.js
+++ Web-Development/JAVASCRIPT/basicJavascript.js
@@ -104,6 +104,7 @@ $(".anchor").on("click", function() {
   let anchorTo = $(this).attr("href");
   //Calculation of the spacing necessary for the object with ID to appear correctly
   let offset = ($(window).width() < 960) ? 200 : 100;
+  
   //Makes the animation following the calculation for the object that contains the ID with the same href value
   $("html,body").animate({ scrollTop: $(anchorTo).offset().top - offset}, "slow");
-});
+}); 
