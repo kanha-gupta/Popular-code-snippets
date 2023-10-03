8f993849312ca9af4253bfa52f9c4fff8c35b503
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2023-10-03 23:27:48 +0530

    added react snippets

---
+++ Web-Development/REACT/Component.jsx
@@ -0,0 +1,20 @@
+// Basic template to create a component in react
+
+//Functional component
+import React from 'react'
+const FunctionalComponent = () => {
+  return (
+    <div>Functional Component</div>
+  )
+}
+export default FunctionalComponent
+
+
+// Arrow component
+import React from 'react'
+
+export const ArrowComponent = () => {
+  return (
+    <div>ArrowComponent</div>
+  )
+}
